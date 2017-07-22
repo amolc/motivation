4f825035419375c3a7fd00ffea388f0ee18e2ca0
amolc <amol.chawathe@fountaintechies.com>
2017-07-22 12:24:02 +0800

    Amol Birthdate

--- js/vars.js
+++ js/vars.js
@@ -1,5 +1,5 @@
 // YOU NEED TO CHANGE THESE VARIABLES //
 
-var sleepingTimeHours = 10, // The hour you sleep. In my case I sleep at 12:00pm
+var sleepingTimeHours = 7, // The hour you sleep. In my case I sleep at 12:00pm
 sleepingTimeMinutes = 00, // The minutes remaining for the time you sleep.
-dateOfBirth = 'September 22 1994';
+dateOfBirth = 'August 15 1978';
